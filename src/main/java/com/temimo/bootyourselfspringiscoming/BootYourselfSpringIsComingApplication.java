f2718d6d17c0ecf91660e7832768a16ad3eb514a
artem <Ivanovav@answer-42.ru>
2023-12-01 13:35:27 +0300

    Почему лучше использовать ConditionalOnMissingBean а не создавать свой бин для перезатирания бина стартера

--- src/main/java/com/temimo/bootyourselfspringiscoming/BootYourselfSpringIsComingApplication.java
+++ src/main/java/com/temimo/bootyourselfspringiscoming/BootYourselfSpringIsComingApplication.java
@@ -3,9 +3,17 @@ package com.temimo.bootyourselfspringiscoming;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class BootYourselfSpringIsComingApplication {
+
+//  Так лучше не делать, ибо бин создается с именем метода, и можем перезатереть то что приходит из стартера,
+//  либо могут поменять название бина, и будет уже 2 бина, а мы расчитывали на один, нужно использовать Primary и т.д.
+//	@Bean
+//	public MyRavenListener ravenListener() {
+//		return new MyRavenListener();
+//	}
 
 	public static void main(String[] args) {
 		ConfigurableApplicationContext run = SpringApplication.run(BootYourselfSpringIsComingApplication.class, args);
